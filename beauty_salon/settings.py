e5412bfaccaf7af3737c4f1a7ddde6847b96a38c
Roman <rombos1978@gmail.com>
2025-01-03 16:56:24 +0300

    fix. DB

--- beauty_salon/settings.py
+++ beauty_salon/settings.py
@@ -135,12 +135,12 @@ STATIC_URL = '/static/'
 # ]
 
 # Путь к директории, куда будут собираться все статические файлы при выполнении collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Дополнительные директории, где Django будет искать статические файлы
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Папка для статических файлов вашего приложения
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # Папка для статических файлов вашего приложения
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
