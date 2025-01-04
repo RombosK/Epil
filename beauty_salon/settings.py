c649ca6c7accae94744134cd51f0ebd8071f66b6
Roman <rombos1978@gmail.com>
2025-01-04 19:18:17 +0300

    commit

--- beauty_salon/settings.py
+++ beauty_salon/settings.py
@@ -87,7 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'laser',
-        'USER': 'postgres'
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
